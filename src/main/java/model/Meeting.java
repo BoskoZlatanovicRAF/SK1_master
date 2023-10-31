c623f451c4972a83db74825babb5105fd9f9daef
nikolaradovic <nradovic4921rn@raf.rs>
2023-10-31 19:59:51 +0100

    Updated meeting spec

--- src/main/java/model/Meeting.java
+++ src/main/java/model/Meeting.java
@@ -4,30 +4,30 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.HashMap;
 
 @Getter
-public abstract class Meeting {
-    private LocalTime timeStart;
-    private LocalTime timeEnd;
+public class Meeting {
+    private LocalDateTime timeStart;
+    private LocalDateTime timeEnd;
     private Room room;
     private HashMap<String, String> additionalAttributes;
 
 
-    public Meeting(LocalTime timeStart, LocalTime timeEnd, Room room) {
+    public Meeting(LocalDateTime timeStart, LocalDateTime timeEnd, Room room) {
         this.timeStart = timeStart;
         this.timeEnd = timeEnd;
         this.room = room;
         this.additionalAttributes = new HashMap<>();
     }
 
-    public Meeting(LocalTime timeStart, LocalTime timeEnd, Room room, HashMap<String, String> additionalAttributes){
+    public Meeting(LocalDateTime timeStart, LocalDateTime timeEnd, Room room, HashMap<String, String> additionalAttributes){
         this.timeStart = timeStart;
         this.timeEnd = timeEnd;
         this.room = room;
         this.additionalAttributes = additionalAttributes;
     }
 
-    protected abstract void isValid();
 }
