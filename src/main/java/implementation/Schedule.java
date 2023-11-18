8ae07481033624073312e409ff7d5af48e1e6c57
nikolaradovic <nradovic4921rn@raf.rs>
2023-11-18 20:28:02 +0100

    added parameter filtering and changed filter method name

--- src/main/java/implementation/Schedule.java
+++ src/main/java/implementation/Schedule.java
@@ -11,7 +11,11 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Getter
 @Setter
 public abstract class Schedule<T> {
@@ -20,6 +24,8 @@ public abstract class Schedule<T> {
     List<LocalDate> exceptions = new ArrayList<>();
     List<Meeting> meetings = new ArrayList<>();
     List<Room> rooms = new ArrayList<>();
+    private LocalTime scheduleTimeStart = LocalTime.of(9,0);
+    private LocalTime scheduleTImeEnd = LocalTime.of(21,0);
 
     public abstract T initSchedule();
 
@@ -31,15 +37,28 @@ public abstract class Schedule<T> {
     }
 
     public abstract T addMeeting(Meeting meeting); //dodavanje novog termina uz provere o zauzetost, obraditi situaciju da je termin već zauzet
-    public abstract T removeMeeting(Meeting meeting);//brisanje zauzetog termina
+    public abstract boolean removeMeeting(Meeting meeting);//brisanje zauzetog termina
 
-    public abstract T filterMeetings(Object... params);
 
-    public abstract Gap filterMeetingsGaps(DayOfWeek dayOfWeek, LocalDate dateStart, LocalDate dateEnd, LocalTime timeStart, LocalTime timeEnd);
-    public abstract Meeting filterMeetings(DayOfWeek dayOfWeek, LocalDate dateStart, LocalDate dateEnd, LocalTime timeStart, LocalTime timeEnd);
+    public List<Meeting> filterMeetingByParameters(HashMap<String,Object> additionalAttributes,String type){
 
-    public abstract Gap filterMeetingsGaps(LocalDateTime timeStart);
-    public abstract Meeting filterMeetings(LocalDateTime timeStart);
+        return this.getMeetings().stream().filter(meeting -> ((Meeting)meeting).hasAdditionalAttribute(additionalAttributes,type))
+                .sorted(Comparator.comparing(Meeting::getTimeStart))
+                .collect(Collectors.toList());
+    }
+    public List<Meeting> filterRoomsByAtribute(HashMap<String,Object> meetingParameters, HashMap<String,Object> roomParameters) {
+        return (List<Meeting>) this.getMeetings().stream().filter(meeting -> ((Meeting)meeting).hasAdditionalAttribute(meetingParameters,"meeting")).filter(m -> ((Meeting)m).hasAdditionalAttribute(roomParameters,"room"))
+                .sorted(Comparator.comparing(Meeting::getTimeStart))
+                .collect(Collectors.toList());
+    }
+
+    public abstract List<Meeting> filterByParameters(HashMap<String,Object> meetingParams,HashMap<String,Object> roomParams);
+
+    public abstract List<Gap> filterMeetingsGapsByTimeSpan(DayOfWeek dayOfWeek, LocalDate dateStart, LocalDate dateEnd, LocalTime timeStart, LocalTime timeEnd);
+    public abstract List<Meeting> filterMeetingsByTimeSpan(DayOfWeek dayOfWeek, LocalDate dateStart, LocalDate dateEnd, LocalTime timeStart, LocalTime timeEnd);
+
+    public abstract List<Gap> filterMeetingsGaps(LocalDateTime timeStart);
+    public abstract List<Meeting> filterMeetings(LocalDateTime timeStart);
 
 
 
