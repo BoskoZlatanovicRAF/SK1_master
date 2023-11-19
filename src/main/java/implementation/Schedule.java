02b67f775f15e21d634e93aa2d9f197996031e64
nikolaradovic <nradovic4921rn@raf.rs>
2023-11-19 13:37:54 +0100

    changed to localDate

--- src/main/java/implementation/Schedule.java
+++ src/main/java/implementation/Schedule.java
@@ -10,10 +10,7 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Getter
@@ -36,8 +33,8 @@ public abstract class Schedule<T> {
         return null;
     }
 
-    public abstract T addMeeting(Meeting meeting); //dodavanje novog termina uz provere o zauzetost, obraditi situaciju da je termin već zauzet
-    public abstract boolean removeMeeting(Meeting meeting);//brisanje zauzetog termina
+    public abstract boolean addMeeting(Meeting meeting);
+
 
 
     public List<Meeting> filterMeetingByParameters(HashMap<String,Object> additionalAttributes,String type){
@@ -52,16 +49,16 @@ public abstract class Schedule<T> {
                 .collect(Collectors.toList());
     }
 
-    public abstract List<Meeting> filterByParameters(HashMap<String,Object> meetingParams,HashMap<String,Object> roomParams);
 
     public abstract List<Gap> filterMeetingsGapsByTimeSpan(DayOfWeek dayOfWeek, LocalDate dateStart, LocalDate dateEnd, LocalTime timeStart, LocalTime timeEnd);
     public abstract List<Meeting> filterMeetingsByTimeSpan(DayOfWeek dayOfWeek, LocalDate dateStart, LocalDate dateEnd, LocalTime timeStart, LocalTime timeEnd);
 
-    public abstract List<Gap> filterMeetingsGaps(LocalDateTime timeStart);
-    public abstract List<Meeting> filterMeetings(LocalDateTime timeStart);
+    public abstract List<Gap> filterMeetingsGaps(LocalDate timeStart);
+    public abstract List<Meeting> filterMeetings(LocalDate timeStart);
 
+    public abstract List<Meeting> filterMeetingsByDateRange(LocalDate startDate, LocalDate endDate);
 
+    public abstract boolean rescheduleMeeting(Meeting meeting, LocalDateTime startTime, LocalDateTime endTime); //premeštanje termina - brisanje i dodavanje novog termina sa istim vezanim podacima
 
-    public abstract T rescheduleMeeting(Meeting meeting, LocalDateTime startTime, LocalDateTime endTime); //premeštanje termina - brisanje i dodavanje novog termina sa istim vezanim podacima
 
 }
