c8ee762456e1ca86152953ea5e3e6c1da6c2f1c0
nikolaradovic <nradovic4921rn@raf.rs>
2023-11-20 22:18:30 +0100

    pushing the updates

--- src/main/java/implementation/Schedule.java
+++ src/main/java/implementation/Schedule.java
@@ -33,10 +33,8 @@ public abstract class Schedule<T> {
         return null;
     }
 
-    public abstract boolean addMeeting(Meeting meeting);
-
-
-    public abstract boolean removeMeeting(Meeting meeting); // maybe change Meeting to the date time span and room or not ??
+    public abstract boolean addMeeting(LocalDateTime startDate, LocalDateTime endDate, String roomName);
+    public abstract boolean addMeeting(LocalDateTime startDate, LocalDateTime endDate, String roomName, HashMap<String, Object> additionalAttributes);
 
 
     public List<Meeting> filterMeetingByParameters(HashMap<String,Object> additionalAttributes,String type){
@@ -61,6 +59,116 @@ public abstract class Schedule<T> {
     public abstract List<Meeting> filterMeetingsByDateRange(LocalDate startDate, LocalDate endDate);
 
     public abstract boolean rescheduleMeeting(Meeting meeting, LocalDateTime startTime, LocalDateTime endTime); //premeštanje termina - brisanje i dodavanje novog termina sa istim vezanim podacima
+    public abstract boolean rescheduleMeeting(LocalDateTime oldStartTime, LocalDateTime oldEndTime, String roomName, LocalDateTime startTime, LocalDateTime endTime);
 
+    private List<Gap> findGaps(Map<Room, List<Meeting>> groupedByRoom,LocalTime startTime,LocalTime finalEndTime) {
+        List<Gap> gaps = new ArrayList<>();
+
+
+
+        for (Room room: groupedByRoom.keySet()){
+
+            LocalTime tempStartTime = startTime;
+
+            List<Meeting> meetings = groupedByRoom.get(room);
+            meetings.sort(Comparator.comparing(Meeting::getTimeStart));
+
+            for (Meeting meeting:meetings){
+
+                if (meeting.getTimeStart().toLocalTime().isAfter(tempStartTime) &&meeting.getTimeStart().toLocalTime().isBefore(finalEndTime)){
+
+                    LocalDateTime timeStart = LocalDateTime.of(meeting.getTimeStart().toLocalDate(),tempStartTime);
+                    LocalDateTime endTime = LocalDateTime.of(meeting.getTimeEnd().toLocalDate(),meeting.getTimeStart().toLocalTime());
+                    Gap gap = new Gap(timeStart, endTime,room);
+                    if (gaps.contains(gap)){
+                        gaps.get(gaps.indexOf(gap)).getRooms().add(room);
+                    }
+                    else {
+                        gaps.add(gap);
+                    }
+
+                }
+                else {
+
+                    Meeting nextMeeting = null;
+                    int index = meetings.indexOf(meeting);
+
+                    if (index < meetings.size()-1){
+
+                        nextMeeting = meetings.get(index+1);
+
+
+                        if (nextMeeting.getTimeStart().toLocalTime().isAfter(tempStartTime)){
+                            LocalDateTime timeStart = LocalDateTime.of(meeting.getTimeStart().toLocalDate(),tempStartTime);
+                            LocalDateTime endTime = meeting.getTimeStart();
+                            Gap gap = new Gap(timeStart, endTime,room);
+
+                            if (gaps.contains(gap)){
+                                gaps.get(gaps.indexOf(gap)).getRooms().add(room);
+                            }
+                            else {
+                                gaps.add(gap);
+                            }
+                        }
+                    }
+
+
+                    else {
+                        LocalDateTime timeStart = null;
+                        if (tempStartTime.equals(startTime)){
+                            timeStart = LocalDateTime.of(meeting.getTimeStart().toLocalDate(),tempStartTime);
+                        }
+                        else {
+                            timeStart = meeting.getTimeEnd();
+                        }
+
+
+                        LocalDateTime endTime = null;
+                        if (meeting.getTimeEnd().toLocalTime().isAfter(finalEndTime)){
+                            tempStartTime = meeting.getTimeEnd().toLocalTime();
+                            break;
+                        }
+                        endTime = LocalDateTime.of(meeting.getTimeEnd().toLocalDate(),finalEndTime);
+                        Gap gap = new Gap(timeStart, endTime,room);
+
+                        if (gaps.contains(gap)){
+                            gaps.get(gaps.indexOf(gap)).getRooms().add(room);
+                        }
+                        else {
+                            gaps.add(gap);
+                        }
+                        tempStartTime = endTime.toLocalTime();
+                        break;
+                    }
+
+                }
+                tempStartTime = meeting.getTimeEnd().toLocalTime();
+            }
+
+            if (tempStartTime.isBefore(finalEndTime)){
+
+                LocalDateTime timeStart = LocalDateTime.of(meetings.get(0).getTimeStart().toLocalDate(),tempStartTime);
+                LocalDateTime endTime = LocalDateTime.of(meetings.get(0).getTimeEnd().toLocalDate(),finalEndTime);
+                Gap gap = new Gap(timeStart, endTime,room);
+
+                if (gaps.contains(gap)){
+                    gaps.get(gaps.indexOf(gap)).getRooms().add(room);
+                }
+                else {
+                    gaps.add(gap);
+                }
+            }
+
+
+        }
+
+        gaps.sort(Comparator.comparing(Gap::getStartTime));
+        return gaps;
+    }
+
+    private Map<Room, List<Meeting>> groupMeetingsByRoom(List<Meeting> meetings) {
+        return meetings.stream()
+                .collect(Collectors.groupingBy(Meeting::getRoom));
+    }
 
 }
